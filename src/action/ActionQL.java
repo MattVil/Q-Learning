c1903308b6d01741b9fcbf66323e58a3f851845c
MattVil <mattvilain@gmail.com>
2017-01-21 21:07:58 +0100

    Implementation of the basic fonctions of ActionQL Class

--- src/action/ActionQL.java
+++ src/action/ActionQL.java
@@ -1,5 +1,29 @@
 package action;
 
-public class ActionQL {
+import behavior.State;
 
+public class ActionQL {
+	private State currentState;
+	private State nextState;
+	
+	private double value;
+	
+	public ActionQL(State current, State next){
+		this.currentState = current;
+		this.nextState = next;
+		
+		this.value = 0;
+	}
+	
+	public double getValue(){
+		return this.value;
+	}
+	
+	public void setValue(double newValue){
+		this.value = newValue;
+	}
+	
+	public State getNextState(){
+		return this.nextState;
+	}
 }
