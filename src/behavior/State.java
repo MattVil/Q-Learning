b27b35a245a07c0f4f7193563c5d4f829cf20f66
MattVil <mattvilain@gmail.com>
2017-01-21 21:07:15 +0100

    Implementation of the basic fonctions of State Class

--- src/behavior/State.java
+++ src/behavior/State.java
@@ -1,5 +1,56 @@
 package behavior;
 
+import java.util.ArrayList;
+
+import action.ActionQL;
+
 public class State {
 
+	private double reward;
+	
+	private int x;
+	private int y;
+	
+	private ArrayList<ActionQL> listAction;
+	
+	public State(int x, int y){
+		this.reward = 0;
+		this.x=x;
+		this.y=y;
+	}
+	
+	
+	
+	public double getReward() {
+		return reward;
+	}
+
+
+
+	public int getX() {
+		return x;
+	}
+
+
+
+	public int getY() {
+		return y;
+	}
+
+
+
+	public ArrayList<ActionQL> getListAction() {
+		return listAction;
+	}
+
+
+
+	public void setReward(double reward) {
+		this.reward = reward;
+	}
+
+	public void setListAction(ArrayList<ActionQL> listAction) {
+		this.listAction = listAction;
+	}
+	
 }
