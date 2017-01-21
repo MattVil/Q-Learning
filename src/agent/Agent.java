3457917e1147c2e8a3e28601ce5f0bb2542fca14
MattVil <mattvilain@gmail.com>
2017-01-21 21:05:59 +0100

    Creation of the Agent part

---
+++ src/agent/Agent.java
@@ -0,0 +1,17 @@
+package agent;
+
+import behavior.State;
+
+public class Agent {
+	private int posX;
+	private int posY;
+	
+	private State currentState;
+	
+	private static double learnFactor = 0.5;
+	private static double discountedFactor = 0.5;
+	
+	public Agent(int x, int y){
+		
+	}
+}
