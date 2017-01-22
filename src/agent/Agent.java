afaab5a7ba52525a91407c3858c90d60a4441796
MattVil <mattvilain@gmail.com>
2017-01-22 17:09:57 +0100

    error of the modification of the agent's state fixed

--- src/agent/Agent.java
+++ src/agent/Agent.java
@@ -12,6 +12,35 @@ public class Agent {
 	private static double discountedFactor = 0.5;
 	
 	public Agent(int x, int y){
-		
+		this.posX = x;
+		this.posY = y;
 	}
+
+	public int getPosX() {
+		return posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public State getCurrentState() {
+		return currentState;
+	}
+
+	public static double getLearnFactor() {
+		return learnFactor;
+	}
+
+	public static double getDiscountedFactor() {
+		return discountedFactor;
+	}
+
+	public void setCurrentState(State currentState) {
+		this.posX = currentState.getX();
+		this.posY = currentState.getY();
+		this.currentState = currentState;
+	}
+	
+	
 }
