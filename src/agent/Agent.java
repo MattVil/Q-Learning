4b06bf4e28e7f0e3aa205f21289274913103f16b
MattVil <mattvilain@gmail.com>
2017-01-26 17:50:57 +0100

    Creation of initialState atribute to restart when reward is found

--- src/agent/Agent.java
+++ src/agent/Agent.java
@@ -6,16 +6,27 @@ public class Agent {
 	private int posX;
 	private int posY;
 	
+	private int initialPosX;
+	private int initialPosY;
+	
 	private State currentState;
 	
 	private static double learnFactor = 0.5;
 	private static double discountedFactor = 0.5;
 	
 	public Agent(int x, int y){
+		this.initialPosX = x;
+		this.initialPosY = y;
+		
 		this.posX = x;
 		this.posY = y;
 	}
 
+	public void backToStart(){
+		posX = initialPosX;
+		posY = initialPosY;
+	}
+	
 	public int getPosX() {
 		return posX;
 	}
