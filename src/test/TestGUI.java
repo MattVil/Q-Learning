2093db57f1688acdff8d6ceae586e53602a23cb7
MattVil <mattvilain@gmail.com>
2017-01-27 20:00:37 +0100

    creation of agent routine

--- src/test/TestGUI.java
+++ src/test/TestGUI.java
@@ -1,9 +1,12 @@
 package test;
 
+import action.ActionQL;
 import agent.Agent;
 import gui.QLGui;
 
 public class TestGUI {
+	
+	private static int speed = 500;
 
 	public static void main(String[] args) {
 		//initialisation of window and map
@@ -18,8 +21,20 @@ public class TestGUI {
 	
 		fenetre.refreshMap();
 		
-		
-		
+		while(!fenetre.getStop()){
+			
+			ActionQL actionChosen = fenetre.getBehavior().QLDecision();
+			fenetre.getBehavior().moveAgent(actionChosen);
+			
+			fenetre.refreshMap();
+			
+			try{
+				Thread.sleep(speed);
+			}catch(InterruptedException e){
+				Thread.currentThread().interrupt();
+				e.printStackTrace();
+			}
+		}
 		
 	}
 
